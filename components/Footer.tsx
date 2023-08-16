ff863476bf90b916e014e0b26bbb772f9a24fd3c
egyyudanugraha <egyyudanugraha@gmail.com>
2023-08-16 22:18:14 +0700

    Add SearchBar Component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
 
         <div className="footer__links">
           {footerLinks.map((link) => (
-            <div key="link.title" className="footer__link">
+            <div key={link.title} className="footer__link">
               <h3 className="font-bold">{link.title}</h3>
               {link.links.map((item) => (
                 <Link key={item.title} href={item.url} className="text-gray-500">
